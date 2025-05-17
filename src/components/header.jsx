894b3f5d8d6676a8c253f2224f9886e2be9d878b
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-05-17 14:46:16 +0300

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1 className="text-2xl"></h1>
+      </div>
+      <div></div>
+    </header>
+  );
+}
---
+++ src/components/main.jsx
@@ -0,0 +1,11 @@
+import Features from "./features";
+import Search from "./search";
+
+export default function Main() {
+  return (
+    <>
+      <Search />
+      <Features />
+    </>
+  );
+}
