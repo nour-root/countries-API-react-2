26e2500e717c9db1b490f4eb34fb1c53d2657bc7
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-05-17 20:09:48 +0300

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -25,15 +25,22 @@ export default function Header() {
     <header className="shadow max-sm:px-6 px-8 py-6 flex items-center justify-between dark:bg-dark-primary">
       <div>
         <h1 className="text-2xl max-sm:text-xl font-bold dark:text-white">
-          where in the world?
+          where in the world ?
         </h1>
       </div>
       <button
         onClick={() => setTheme(!theme)}
         className="flex items-center space-x-2 cursor-pointer dark:text-white"
       >
-        <IoMoonOutline className="text-xl" />
-        <p className="text-xl font-semibold max-sm:text-lg">dark Mode</p>
+        {theme ? (
+          <GoSun className="text-xl dark:text-white" />
+        ) : (
+          <IoMoonOutline className="text-xl" />
+        )}
+
+        <p className="text-xl font-semibold max-sm:text-lg capitalize">
+          {theme ? "light" : "dark"} Mode
+        </p>
       </button>
     </header>
   );
